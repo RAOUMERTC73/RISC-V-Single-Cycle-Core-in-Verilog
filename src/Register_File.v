c4fadba2cb0ab066bb3dde685960de8b00d06ae9
RAOUMERTC73 <raoumer160903@gmail.com>
2024-08-12 03:01:39 +0500

    update Register_File.v

--- src/Register_File.v
+++ src/Register_File.v
@@ -3,7 +3,7 @@ module Register_File(clk,rst,WE3,WD3,A1,A2,A3,RD1,RD2);
     input clk,rst,WE3;
     input [4:0]A1,A2,A3;
     input [31:0]WD3;
-    output [31:0]RD1,RD2;
+    output [31:0]RD1,RD2; 
 
     reg [31:0] Register [31:0];
 
@@ -17,8 +17,14 @@ module Register_File(clk,rst,WE3,WD3,A1,A2,A3,RD1,RD2);
     assign RD2 = (~rst) ? 32'd0 : Register[A2];
 
     initial begin
-        Register[5] = 32'h00000005;
-        Register[6] = 32'h00000004;
+    //  Register[9] = 32'h00000020;  //LW
+    //  Register[6] = 32'h00000040;  //LW
+    //  Register[11] = 32'h00000028; //SW
+    //  Register[12] = 32'h00000030; //SW
+    //  Register[5] = 32'h00000006; //R TYPE
+    //  Register[6] = 32'h0000000A; // R TYPE
+     Register[5] = 32'h00000005; //R TYPE
+     Register[6] = 32'h00000004; // R TYPE
         
     end
 
